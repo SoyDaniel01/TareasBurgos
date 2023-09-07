5a343e1a84e534637ad7ef17422f83cde91660c8
DanielSotelo <danielescesc@gmail.com>
2023-09-07 12:17:23 -0700

    medio le avance al p1

---
+++ unidad4/p1-abxy.java
@@ -0,0 +1,16 @@
+class hallarY{
+    main(){
+        //Estructura de controlo
+        float a, b, x, y;
+
+        //Entrada(lectura)
+        imprimir("Cual es el valor de a?..."); leer a;
+        imprimir("Cual es el valor de b?..."); leer b;
+        imprimir("Cual es el valor de X?..."); leer x;
+
+        //Proceso (calculo)
+        
+
+        // Salida (Resultados)
+    }
+}
